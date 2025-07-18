c30ed87c2636dfc8440918874f07acd0482bbd08
Maaaksl87 <maaaksl87@gmail.com>
2025-07-18 23:42:08 +0300

    just organized folder structure, delete some files trash

--- main.js
+++
@@ -1,6 +0,0 @@
-const toggleBtn = document.getElementById('toggle');
-const sidebar = document.querySelector('.sidebar');
-
-toggleBtn.addEventListener('click', () => {
-    sidebar.classList.toggle('hidden');
-})
